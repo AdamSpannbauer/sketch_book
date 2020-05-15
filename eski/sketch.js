59677c13c06662be781db1bcaf7573ab8706ffe5
AdamSpannbauer <spannbaueradam@gmail.com>
2020-05-15 19:02:56 -0400

    add eski text animation

---
+++ eski/sketch.js
@@ -0,0 +1,48 @@
+const canvas_w = 512;
+const canvas_h = 512;
+
+const yellow = [253, 227, 117];
+const pink = [230, 132, 135];
+const blue = [59, 98, 151];
+
+let font;
+
+const off = 8;
+const y = -10;
+
+// const display_text = 'eski';
+const display_text = 'skinny\ncircuits'
+const speed = 0.3;
+
+function preload() {
+  // font = loadFont('assets/Republica_Minor_2_0.otf');
+  font = loadFont('assets/VCR_OSD_MONO.ttf');
+}
+
+
+function setup() {
+  createCanvas(canvas_w, canvas_h);
+}
+
+
+function writeText(display_text, max_x, y, c) {
+  fill(c);
+  const x = min([-off + frameCount * speed, max_x]);
+  text(display_text, x, y);
+}
+
+
+function draw() {
+  background(20);
+
+  textFont(font);
+  textSize(100);
+  textAlign(CENTER);
+
+  translate(width / 2, height / 2);
+  noStroke();
+  
+  writeText(display_text, -off, y, blue);
+  writeText(display_text, 0, y, pink);
+  writeText(display_text, off, y, yellow);
+}
